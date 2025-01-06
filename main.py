e48fa23989bd7d40adddd82d174d465591e141da
11011san <11011san@mail.com>
2025-01-06 10:42:16 +0100

    cleand up the print of char frequency

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-def main():
-    text = get_text("books/frankenstein.txt")
-    print(f"There was {word_count(text)} words in this file")
-    print(f"Chars fequense:")
-    print(count_char(text))
+import re
+
+
+def main(path):
+    text = get_text(path)
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count(text)} words found in the document")
+    print(f"")
+    print_char_frequency(text)
+    print(f"--- End report ---")
 
 def word_count(text):
     return len(text.split())
@@ -13,9 +18,15 @@ def count_char(text):
         dic[ch] = dic.setdefault(ch, 0) + 1
     return dic
 
+def print_char_frequency(text):
+    dic = count_char(text)
+    list = sorted(dic.items(), reverse=True, key=lambda x: x[1])
+    for ch, freq in list:
+        if ch.isalpha():
+            print(f"The '{ch}' character was found {freq} times")
 
 def get_text(path):
     with open(path) as f:
         return f.read()
 
-main()
+main("books/frankenstein.txt")
